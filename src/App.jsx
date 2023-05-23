985e34fcf8fb5a195617698c4bbb6e4b65e5288e
mhid-rethink <matheus.dias@rethink.dev>
2023-05-23 15:20:06 -0300

    reorganizando pastas

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
-import Home from "./screens/Home";
-import Products from "./screens/Products";
-import Contacts from "./screens/Contacts";
+import Home from "./screens/Home/Home";
+import Products from "./screens/Products/Products";
+import Contacts from "./screens/Contacts/Contacts";
+import ProductDetails from "./screens/ProductDetails/ProductDetails";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
           <Route path="/contacts" element={<Contacts />} />
         </Routes>
         <Footer />
